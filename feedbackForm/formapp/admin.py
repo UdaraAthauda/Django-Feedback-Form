b14a47b19b30024ca314076e9649e9c96cba6602
UdaraAthauda <140876466+UdaraAthauda@users.noreply.github.com>
2024-05-14 21:25:50 +0530

    Update admin.py

--- feedbackForm/formapp/admin.py
+++ feedbackForm/formapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import *
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
+admin.site.register(Feedback, ImportExportModelAdmin)
